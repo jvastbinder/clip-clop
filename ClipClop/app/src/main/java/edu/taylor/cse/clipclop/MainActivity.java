bdd84a27f5f9a93d820767a430e5821f61f3656b
Andrew Blomenberg <andrewBlomen@gmail.com>
2017-11-09 14:49:20 -0500

    Created BufferNotif Class with starter code

--- ClipClop/app/src/main/java/edu/taylor/cse/clipclop/MainActivity.java
+++ ClipClop/app/src/main/java/edu/taylor/cse/clipclop/MainActivity.java
@@ -16,36 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        final Runnable task = new Runnable() {
+//            @Override
+//            public void run() {
+                BufferNotif notif= new BufferNotif();
 
+                notif.notify(this,"Potato",3);      //code you want to run every second
+//            }
+//        };
 
-        Notification.Builder mBuilder = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.clip)
-                .setContentTitle("Buffer Interface")
-                .setContentText("This is how the Buffer will be accessed")
-                .setOngoing(true);
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, MainActivity.class);
-
-        // The stack builder object will contain an artificial back stack for the started Activity.
-        // This ensures that navigating backward from the Activity leads out of your app to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-
-        // mNotificationId is a unique integer your app uses to identify the
-        // notification. For example, to cancel the notification, you can pass its ID
-        // number to NotificationManager.cancel().
-        mNotificationManager.notify(0, mBuilder.build());
+ //       task.run();
 
     }
 }
