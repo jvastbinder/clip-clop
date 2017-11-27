c4ef8299f5eafc5896444379efba2dec39cd554b
joel <joel_vastbinder@taylor.edu>
2017-11-27 18:08:16 -0500

    Made typing work to change bufferSize variable

--- ClipClop/app/src/main/java/edu/taylor/cse/clipclop/MainActivity.java
+++ ClipClop/app/src/main/java/edu/taylor/cse/clipclop/MainActivity.java
@@ -2,6 +2,8 @@ package edu.taylor.cse.clipclop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.view.View;
 
@@ -17,7 +19,25 @@ int bufferSize = 5;
         setContentView(R.layout.activity_main);
 
         LinkedList<String> buffer;
+        EditText bufferSizeDisplay = (EditText) findViewById(R.id.editText);
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                EditText bufferSizeDisplay = (EditText) findViewById(R.id.editText);
+                bufferSize = Integer.parseInt(bufferSizeDisplay.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+
+        bufferSizeDisplay.addTextChangedListener(watcher);
 
         //Add copied string to clipboard by initializing listener
         ClipboardListener cliplistener = new ClipboardListener(getApplicationContext());
